6a5d3e2eaa3f2e268420d7b11318e8766115c792
KurtMerbeth <Kurt@Merbeth.io>
2023-05-13 17:02:28 +0200

    adjusted colors

--- app/src/other/utils.js
+++ app/src/other/utils.js
@@ -17,13 +17,17 @@ export function stringToColor2(str) {
   return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 }
 
-export function stringToColor(str) {
+export function stringToColor(text) {
+  let str = text;
   let hash = 0;
   for (let i = 0; i < str.length / 2; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
   }
-  const r = Math.floor((Math.abs(Math.sin(hash + 0)) * 256) % 256);
-  const g = Math.floor((Math.abs(Math.sin(hash + 1)) * 256) % 256);
-  const b = Math.floor((Math.abs(Math.sin(hash + 2)) * 256) % 256);
-  return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}aa`;
+  const r = Math.floor((Math.abs(Math.sin(hash+0)) * 256) % 256);
+  const g = Math.floor((Math.abs(Math.sin(hash+1)) * 256) % 256);
+  const b = Math.floor((Math.abs(Math.sin(hash+2)) * 256) % 256);
+
+  // modulo function on str.length to chose between aa, bb, cc, dd
+  const append = ["88", "aa", "66", "cc"][str.length % 4];
+  return `#${r.toString(16)}${g.toString(16)}${b.toString(16)}${append}`;
 }
